3d12a11c6980d4bd8e14d16a73c090034aa0b449
Tristan (t4t5) <t4t5@hey.com>
2025-11-22 15:54:10 +0000

    Add dialog component

--- src/components/ActionBar.tsx
+++ src/components/ActionBar.tsx
@@ -1,10 +1,27 @@
+import { useState } from "react"
+
 import { Button } from "@/components/ui/button"
+import { Modal } from "@/components/ui/dialog"
 
 export function ActionBar() {
+  const [showModal, setShowModal] = useState(false)
+
   return (
     <div className="p-4 flex justify-between">
       <Button variant="secondary">+</Button>
-      <Button variant="secondary">Search</Button>
+
+      <div className="flex gap-2">
+        {showModal && (
+          <Modal onClose={() => setShowModal(false)}>
+            <h1>Test</h1>
+          </Modal>
+        )}
+
+        <Button variant="secondary" onClick={() => setShowModal(true)}>
+          Settings
+        </Button>
+        <Button variant="secondary">Search</Button>
+      </div>
     </div>
   )
 }
