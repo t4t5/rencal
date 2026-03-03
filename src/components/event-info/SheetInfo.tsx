0a41f2a6a6f30f9ac48a5a2455015a27aaff9fb9
Tristan (t4t5) <t4t5@hey.com>
2026-03-03 17:54:23 +0000

    Improve performance of sheet animation

--- src/components/event-info/SheetInfo.tsx
+++ src/components/event-info/SheetInfo.tsx
@@ -1,3 +1,5 @@
+import { useDeferredValue } from "react"
+
 import { EditEvent } from "@/components/event-info/EditEvent"
 import { Sheet, SheetContent, SheetDescription, SheetTitle } from "@/components/ui/sheet"
 
@@ -5,13 +7,14 @@ import { useCalEvents } from "@/contexts/CalEventsContext"
 
 export function SheetEvent() {
   const { activeEvent, setActiveEventId } = useCalEvents()
+  const deferredEvent = useDeferredValue(activeEvent)
 
   return (
     <Sheet open={!!activeEvent} onOpenChange={() => setActiveEventId(null)}>
       <SheetContent onOpenAutoFocus={(e) => e.preventDefault()}>
         <SheetTitle className="hidden">{activeEvent?.summary}</SheetTitle>
         <SheetDescription className="hidden">{activeEvent?.summary}</SheetDescription>
-        <EditEvent event={activeEvent} />
+        {deferredEvent && <EditEvent event={deferredEvent} />}
       </SheetContent>
     </Sheet>
   )
