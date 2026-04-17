1c511c2f1a5ff0e376b093913580f41b1bca3408
Tristan (t4t5) <t4t5@hey.com>
2026-04-17 07:39:41 +0100

    Only allow editable calendars when composing events

--- src/components/event-info/inputs/CalendarSelect.tsx
+++ src/components/event-info/inputs/CalendarSelect.tsx
@@ -18,13 +18,15 @@ export const CalendarSelect = ({
 }) => {
   const { calendars } = useCalendars()
 
+  const editableCalendars = calendars.filter((cal) => !cal.read_only)
+
   return (
     <Select onValueChange={onChange}>
       <SelectTrigger className={cn("w-full", readOnly && "pointer-events-none")}>
         {calendar ? <CalendarItem calendar={calendar} /> : <span>Select Calendar</span>}
       </SelectTrigger>
       <SelectContent>
-        {calendars.map((cal) => (
+        {editableCalendars.map((cal) => (
           <SelectItem key={cal.slug} value={cal.slug}>
             <CalendarItem calendar={cal} />
           </SelectItem>
