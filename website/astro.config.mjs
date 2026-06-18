f6bc3bb66af1fa77dc4e71972c88e26d6aa21cc3
Tristan (t4t5) <t4t5@hey.com>
2026-06-18 12:57:18 +0100

    Add logotype + tailwind to astro

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,8 +1,12 @@
 import starlight from "@astrojs/starlight"
+import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "astro/config"
 import starlightThemeRapide from "starlight-theme-rapide"
 
 export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
   integrations: [
     starlight({
       title: "renCal",
