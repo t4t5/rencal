6b6ebea4891d4719ece2a60b0b9caa4ea337ae9b
Tristan (t4t5) <t4t5@hey.com>
2026-07-04 10:44:34 +0100

    Make sure weeks in DatePicker start on Monday
    For consistency with the rest of the calendars

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -26,6 +26,7 @@ function Calendar({
   return (
     <DayPicker
       showOutsideDays={showOutsideDays}
+      weekStartsOn={1}
       className={cn(
         "bg-background group/calendar p-3 [--cell-size:--spacing(8)] [[data-slot=card-content]_&]:bg-transparent [[data-slot=popover-content]_&]:bg-transparent",
         String.raw`rtl:**:[.rdp-button\_next>svg]:rotate-180`,
