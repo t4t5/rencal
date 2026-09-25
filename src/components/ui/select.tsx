76bce4f85e6750e233155f80275f81847836cfd5
Tristan (t4t5) <t4t5@hey.com>
2026-09-25 15:55:26 +0100

    Support end-to-end select items

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -125,6 +125,7 @@ function SelectContent({
       >
         <SelectScrollUpButton />
         <SelectPrimitive.Viewport
+          data-slot="select-viewport"
           className={cn(
             "p-1",
             position === "popper" &&
